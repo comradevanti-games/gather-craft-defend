a1f5c3946c887eae33f525100de1c3e65610b3f1
Michael Zauchinger <55690191+Mumb0@users.noreply.github.com>
2022-10-01 16:26:44 +0200

    add gatherpoint

---
+++ Assets/Scripts/Runtime/GatherPoints/GatherPoint.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace GatherCraftDefend.GatherPoints {
+
+	public class GatherPoint : MonoBehaviour {
+
+#region Fields
+
+		[SerializeField] private Collider2D gatherRangeTrigger;
+
+#endregion
+
+#region Properties
+		
+		
+
+#endregion
+
+#region Methods
+
+#endregion
+
+	}
+
+}
