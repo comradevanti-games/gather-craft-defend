b80e0597703050e920de10833b07c352d9226e2b
ComradeVanti <kingvanti650@gmail.com>
2022-10-02 10:25:07 +0200

    feat: enemies have health

---
+++ Assets/Scripts/Runtime/HealthKeeper.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GatherCraftDefend
+{
+
+    public class HealthKeeper : MonoBehaviour
+    {
+
+        [SerializeField] private UnityEvent<int> onHealthChanged;
+        
+        private int health;
+
+
+        public int Health
+        {
+            get => health;
+            set
+            {
+                if (health != value)
+                {
+                    onHealthChanged.Invoke(value);
+                    health = value;
+                }
+            }
+        }
+
+        public UnityEvent<int> OnHealthChanged => onHealthChanged;
+
+    }
+
+}
