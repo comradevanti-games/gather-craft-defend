07514067bc03653448615c005a84e8920d8276a3
ComradeVanti <kingvanti650@gmail.com>
2022-10-02 08:47:28 +0200

    feat: smooth camera

---
+++ Assets/Scripts/Runtime/SmoothTransformFollow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace GatherCraftDefend
+{
+
+    public class SmoothTransformFollow : MonoBehaviour
+    {
+
+        [SerializeField] [Range(0.1f, 2f)] private float targetReachTime;
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private Transform target;
+
+        private Vector3 velocity = Vector3.zero;
+
+
+        private Vector3 CurrentPos
+        {
+            get => transform.position;
+            set => transform.position = value;
+        }
+
+        private Vector3 TargetPos => target.position + offset;
+
+
+        private void Update() =>
+            UpdatePosition();
+
+        private void UpdatePosition() =>
+            CurrentPos = Vector3.SmoothDamp(CurrentPos, TargetPos, ref velocity, targetReachTime);
+
+    }
+
+}
