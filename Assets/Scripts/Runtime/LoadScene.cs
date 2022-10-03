857e1f1f87ce0e769a4a57b50865d25d2401b9cd
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2022-10-03 14:48:09 +0200

    add main menu

--- Assets/Scripts/Runtime/LoadScene.cs
+++ Assets/Scripts/Runtime/LoadScene.cs
@@ -5,83 +5,75 @@ using ComradeVanti.CSharpTools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace GatherCraftDefend
-{
+namespace GatherCraftDefend {
 
-    public class LoadScene : MonoBehaviour
-    {
+	public class LoadScene : MonoBehaviour {
 
-        private const string PlayerPrefKey = "SceneLoadParams";
+		private const string PlayerPrefKey = "SceneLoadParams";
 
-        public static SceneLoadParams Parameters => LoadFromPlayerPrefs();
-        
+		public static SceneLoadParams Parameters => LoadFromPlayerPrefs();
 
-        public void WithName(string sceneName) =>
-            WithName(sceneName, SceneLoadParams.empty);
+		public void WithName(string sceneName) =>
+			WithName(sceneName, SceneLoadParams.empty);
 
-        public void WithName(string sceneName, SceneLoadParams parameters)
-        {
-            WriteToPlayerPrefs(parameters);
-            SceneManager.LoadScene(sceneName);
-        }
+		public void WithName(string sceneName, SceneLoadParams parameters) {
+			WriteToPlayerPrefs(parameters);
+			SceneManager.LoadScene(sceneName);
+		}
 
-        private static void WriteToPlayerPrefs(SceneLoadParams parameters)
-        {
-            var entryStrings =
-                parameters.keyValuePairs
-                          .Select(entry => $"{entry.Key}={entry.Value}");
-            var pref = string.Join(",", entryStrings);
-            PlayerPrefs.SetString(PlayerPrefKey, pref);
-        }
+		public void QuitGame() {
+			Application.Quit();
+		}
 
-        private static SceneLoadParams LoadFromPlayerPrefs()
-        {
-            var pref = PlayerPrefs.GetString(PlayerPrefKey);
-            var entryStrings = pref.Split(",", StringSplitOptions.RemoveEmptyEntries);
+		private static void WriteToPlayerPrefs(SceneLoadParams parameters) {
+			var entryStrings =
+				parameters.keyValuePairs
+					.Select(entry => $"{entry.Key}={entry.Value}");
+			var pref = string.Join(",", entryStrings);
+			PlayerPrefs.SetString(PlayerPrefKey, pref);
+		}
 
-            SceneLoadParams AddEntry(SceneLoadParams parameters, string entryString)
-            {
-                var parts = entryString.Split("=");
-                var key = parts[0];
-                var value = parts[1];
+		private static SceneLoadParams LoadFromPlayerPrefs() {
+			var pref = PlayerPrefs.GetString(PlayerPrefKey);
+			var entryStrings = pref.Split(",", StringSplitOptions.RemoveEmptyEntries);
 
-                return parameters.AddEntry(key, value);
-            }
+			SceneLoadParams AddEntry(SceneLoadParams parameters, string entryString) {
+				var parts = entryString.Split("=");
+				var key = parts[0];
+				var value = parts[1];
 
-            return entryStrings.Aggregate(SceneLoadParams.empty, AddEntry);
-        }
+				return parameters.AddEntry(key, value);
+			}
 
-    }
+			return entryStrings.Aggregate(SceneLoadParams.empty, AddEntry);
+		}
 
+	}
 
-    public class SceneLoadParams
-    {
+	public class SceneLoadParams {
 
-        public static readonly SceneLoadParams empty =
-            new SceneLoadParams(ImmutableDictionary<string, string>.Empty);
+		public static readonly SceneLoadParams empty =
+			new SceneLoadParams(ImmutableDictionary<string, string>.Empty);
 
-        public readonly ImmutableDictionary<string, string> keyValuePairs;
+		public readonly ImmutableDictionary<string, string> keyValuePairs;
 
+		private SceneLoadParams(ImmutableDictionary<string, string> keyValuePairs) =>
+			this.keyValuePairs = keyValuePairs;
 
-        private SceneLoadParams(ImmutableDictionary<string, string> keyValuePairs) =>
-            this.keyValuePairs = keyValuePairs;
+		public SceneLoadParams AddEntry(string key, string value) =>
+			new SceneLoadParams(keyValuePairs.Add(key, value));
 
+		private SceneLoadParams AddValue(string name, string type, string value) =>
+			AddEntry($"{type}:{name}", value);
 
-        public SceneLoadParams AddEntry(string key, string value) =>
-            new SceneLoadParams(keyValuePairs.Add(key, value));
+		public SceneLoadParams AddInt(string name, int i) =>
+			AddValue(name, "int", i.ToString());
 
-        private SceneLoadParams AddValue(string name, string type, string value) =>
-            AddEntry($"{type}:{name}", value);
+		public IOpt<int> TryGetInt(string name) {
+			var key = $"int:{name}";
+			return keyValuePairs.TryGet(key).Map(int.Parse);
+		}
 
-        public SceneLoadParams AddInt(string name, int i) =>
-            AddValue(name, "int", i.ToString());
-
-        public IOpt<int> TryGetInt(string name)
-        {
-            var key = $"int:{name}";
-            return keyValuePairs.TryGet(key).Map(int.Parse);
-        }
-
-    }
+	}
 
 }
